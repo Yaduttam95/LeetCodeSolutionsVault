84f05b2dc97990c4ec9140668f98321084932208
Yaduttam Pareek <yaduttampareek95@gmail.com>
2025-02-08 09:03:21 +0530

    Time: 55 ms (96.58%) | Memory: 130.2 MB (8.90%) - LeetSync

---
+++ 2434-design-a-number-container-system/design-a-number-container-system.java
@@ -0,0 +1,35 @@
+class NumberContainers {
+
+    HashMap<Integer,PriorityQueue<Integer>> res;
+    HashMap<Integer,Integer> index_val;
+
+    public NumberContainers() {
+        res = new HashMap<>();
+        index_val = new HashMap<>();
+    }
+    
+    public void change(int index, int number) {
+
+        if(index_val.containsKey(index)){
+            int num = index_val.get(index);
+            if(num == number){
+                return ;
+            }
+
+            res.get(num).remove(index);
+        }
+
+        res.computeIfAbsent(number, k-> new PriorityQueue<>()).offer(index);
+        index_val.put(index, number);
+    }
+    
+    public int find(int number) 
+    {
+        PriorityQueue<Integer> pq = res.getOrDefault(number, new PriorityQueue<>());
+        if(pq.size() == 0)
+        {
+            return -1;
+        }
+        return pq.peek();
+    }
+}
